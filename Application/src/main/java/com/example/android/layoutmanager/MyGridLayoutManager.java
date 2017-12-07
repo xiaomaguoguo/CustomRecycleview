b64a36b46778adae30544ca90c0448401dcfe480
mazhihua <mazhihua@bftv.com>
2017-12-07 10:28:47 +0800

    再来一波

--- Application/src/main/java/com/example/android/layoutmanager/MyGridLayoutManager.java
+++ Application/src/main/java/com/example/android/layoutmanager/MyGridLayoutManager.java
@@ -1,8 +1,43 @@
 package com.example.android.layoutmanager;
 
+import android.content.Context;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+
 /**
  * Created by MaZhihua on 2017/12/7.
  */
 
-public class MyGridLayoutManager {
+public class MyGridLayoutManager extends GridLayoutManager{
+
+    public MyGridLayoutManager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public MyGridLayoutManager(Context context, int spanCount) {
+        super(context, spanCount);
+    }
+
+    public MyGridLayoutManager(Context context, int spanCount, int orientation, boolean reverseLayout) {
+        super(context, spanCount, orientation, reverseLayout);
+    }
+
+    /*@Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        final RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+        if (layoutManager instanceof GridLayoutManager) {
+            final GridLayoutManager gridManager = ((GridLayoutManager) layoutManager);
+            gridManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+                @Override
+                public int getSpanSize(int position) {
+                    if (isFooterView(position)) {
+                        return gridManager.getSpanCount();
+                    }
+                    return 1;
+                }
+            });
+        }
+    }*/
 }
--- Application/src/main/java/com/example/android/layoutmanager/MyStaggeredGridLayoutManager.java
+++ Application/src/main/java/com/example/android/layoutmanager/MyStaggeredGridLayoutManager.java
@@ -1,8 +1,36 @@
 package com.example.android.layoutmanager;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
 /**
  * Created by MaZhihua on 2017/12/7.
  */
 
-public class MyStaggeredGridLayoutManager {
+public class MyStaggeredGridLayoutManager extends StaggeredGridLayoutManager {
+
+    public MyStaggeredGridLayoutManager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    public MyStaggeredGridLayoutManager(int spanCount, int orientation) {
+        super(spanCount, orientation);
+    }
+
+    /*@Override
+    public void onViewAttachedToWindow(RecyclerView.ViewHolder holder) {
+        super.onViewAttachedToWindow(holder);
+        if (isFooterView(holder.getLayoutPosition())) {
+            ViewGroup.LayoutParams lp = holder.itemView.getLayoutParams();
+
+            if (lp != null && lp instanceof StaggeredGridLayoutManager.LayoutParams) {
+                StaggeredGridLayoutManager.LayoutParams p = (StaggeredGridLayoutManager.LayoutParams) lp;
+                p.setFullSpan(true);
+            }
+        }
+    }*/
+
 }
